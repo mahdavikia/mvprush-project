f7a5b0b30e85fef6eb7be61b98bcedcd4b0f587d
Mohammad Mahdavi Kia <mahdavikia.m@gmail.com>
2025-06-09 08:47:07 +0330

    Fixed menu in view

--- storage/framework/views/04256ca7d14c98688591303e2a30ab92.php
+++ storage/framework/views/04256ca7d14c98688591303e2a30ab92.php
@@ -1,43 +1,44 @@
-@extends('admin.layout')
-@if(Melorain::getAccess('UserController@active'))
-@section('header')
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-@endsection
-@endif
-@section('content')
+<?php if(Melorain::getAccess('UserController@active')): ?>
+<?php $__env->startSection('header'); ?>
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>" />
+<?php $__env->stopSection(); ?>
+<?php endif; ?>
+<?php $__env->startSection('content'); ?>
     <div class="example">
         <nav class="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="#">مدیریت</a></li>
-                <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">مدیران سیستم</a></li>
+                <li class="breadcrumb-item"><a href="<?php echo e(route('admin.users.index')); ?>">مدیران سیستم</a></li>
                 <li class="breadcrumb-item active" aria-current="page">لیست مدیران</li>
             </ol>
         </nav>
     </div>
 
-    @if($message = Session::get('error') || $message = Session::get('success'))
+    <?php if($message = Session::get('error') || $message = Session::get('success')): ?>
     <div class="row">
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    @if ($message = Session::get('error'))
+                    <?php if($message = Session::get('error')): ?>
                         <div class="note note-danger">
                             <strong>خطا!</strong>
                             <br>
-                            {{ $message }}
+                            <?php echo e($message); ?>
+
                         </div>
-                    @endif
-                    @if ($message = Session::get('success'))
+                    <?php endif; ?>
+                    <?php if($message = Session::get('success')): ?>
                         <div class="note note-success">
-                            {{ $message }}
+                            <?php echo e($message); ?>
+
                         </div>
-                    @endif
+                    <?php endif; ?>
 
                 </div>
             </div>
         </div>
     </div>
-    @endif
+    <?php endif; ?>
 
 
     <div class="row">
@@ -74,28 +75,28 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($users as $user)
+                                <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                 <tr>
                                     <td>
                                         <div class="dropdown mb-2">
-                                            <a type="button" id="dropdownMenuButton_{{$user->id}}" data-bs-toggle="dropdown" aria-haspopup="true"
+                                            <a type="button" id="dropdownMenuButton_<?php echo e($user->id); ?>" data-bs-toggle="dropdown" aria-haspopup="true"
                                                aria-expanded="false">
                                                 <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
                                             </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton_{{$user->id}}">
-                                                {{--                                            <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">مشاهده</span></a>--}}
-                                                <a class="dropdown-item d-flex align-items-center" href="{{route('admin.users.edit',$user)}}"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">ویرایش</span></a>
-                                                @if(Auth::user()->role_id == 1)
-                                                <a class="dropdown-item d-flex align-items-center" href="javascript:;" data-bs-toggle="modal" data-bs-target="#modal_{{$user->id}}"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">حذف</span></a>
-                                                @endif
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton_<?php echo e($user->id); ?>">
+                                                
+                                                <a class="dropdown-item d-flex align-items-center" href="<?php echo e(route('admin.users.edit',$user)); ?>"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">ویرایش</span></a>
+                                                <?php if(Auth::user()->role_id == 1): ?>
+                                                <a class="dropdown-item d-flex align-items-center" href="javascript:;" data-bs-toggle="modal" data-bs-target="#modal_<?php echo e($user->id); ?>"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">حذف</span></a>
+                                                <?php endif; ?>
 
                                             </div>
                                         </div>
 
-                                        <div class="modal fade" id="modal_{{$user->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <form class="me-0 w-full" action="{{ route('admin.users.destroy',$user) }}" method="POST" >
-                                                @csrf
-                                                @method('DELETE')
+                                        <div class="modal fade" id="modal_<?php echo e($user->id); ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <form class="me-0 w-full" action="<?php echo e(route('admin.users.destroy',$user)); ?>" method="POST" >
+                                                <?php echo csrf_field(); ?>
+                                                <?php echo method_field('DELETE'); ?>
                                                 <div class="modal-dialog modal-dialog-centered">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -115,43 +116,47 @@
                                         </div>
                                     </td>
                                     <td>
-                                        {{$user->role->title}}
+                                        <?php echo e($user->role->title); ?>
+
                                     </td>
                                     <td>
-                                        <a href="{{route('admin.users.edit',$user)}}">{{$user->profile->full_name}}</a>
+                                        <a href="<?php echo e(route('admin.users.edit',$user)); ?>"><?php echo e($user->profile->full_name); ?></a>
                                     </td>
                                     <td>
-                                        {{$user->email}}
+                                        <?php echo e($user->email); ?>
+
                                     </td>
                                     <td>
-                                        {{$user->created_at->jdate('Y/m/d')}}
+                                        <?php echo e($user->created_at->jdate('Y/m/d')); ?>
+
                                     </td>
                                     <td>
-                                        @if(Melorain::getAccess('UserController@active'))
+                                        <?php if(Melorain::getAccess('UserController@active')): ?>
                                             <div class="form-check form-switch mb-2">
-                                                <input type="checkbox" name="news_active" onchange="callActive({{$user->id}})" value="1" class="form-check-input" @if($user->activated == '1') checked @endif id="formSwitch{{$user->id}}">
-                                                <label class="form-check-label" for="formSwitch{{$user->id}}">فعال</label>
+                                                <input type="checkbox" name="news_active" onchange="callActive(<?php echo e($user->id); ?>)" value="1" class="form-check-input" <?php if($user->activated == '1'): ?> checked <?php endif; ?> id="formSwitch<?php echo e($user->id); ?>">
+                                                <label class="form-check-label" for="formSwitch<?php echo e($user->id); ?>">فعال</label>
                                             </div>
-                                        @else
+                                        <?php else: ?>
                                             <div class="form-check form-switch mb-2" title="دسترسی ندارید">
-                                                <input type="checkbox" name="news_active" disabled class="form-check-input" @if($user->activated == '1') checked @endif id="formSwitch{{$user->id}}">
-                                                <label class="form-check-label" for="formSwitch{{$user->id}}">فعال</label>
+                                                <input type="checkbox" name="news_active" disabled class="form-check-input" <?php if($user->activated == '1'): ?> checked <?php endif; ?> id="formSwitch<?php echo e($user->id); ?>">
+                                                <label class="form-check-label" for="formSwitch<?php echo e($user->id); ?>">فعال</label>
                                             </div>
-                                        @endif
+                                        <?php endif; ?>
                                     </td>
                                 </tr>
-                                @endforeach
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                 </tbody>
                             </table>
                     </div>
                 </div>
             </div>
         </div>
-        {{ $users->links() }}
+        <?php echo e($users->links()); ?>
+
         </div>
-@endsection
-@if(Melorain::getAccess('UserController@active'))
-@section('footer')
+<?php $__env->stopSection(); ?>
+<?php if(Melorain::getAccess('UserController@active')): ?>
+<?php $__env->startSection('footer'); ?>
 
     <script type="text/javascript">
 
@@ -172,7 +177,7 @@
                 };
             }
 
-            $.post('{{route('admin.users.active')}}',data,
+            $.post('<?php echo e(route('admin.users.active')); ?>',data,
                 function(data, status){
                     if(data === '1' && status == 200){
                         const Toast = Swal.mixin({
@@ -203,5 +208,6 @@
         }
 
     </script>
-@endsection
-@endif
+<?php $__env->stopSection(); ?>
+<?php endif; ?>
+<?php echo $__env->make('admin.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\wamp64_new\www\mvprush\_git\resources\views/admin/users/index.blade.php ENDPATH**/ ?>
